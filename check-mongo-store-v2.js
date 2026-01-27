a7b84fcea37e226f2876a4dc88f655de13cbc147
Md Monim Islam <monimmdmonim41@gmail.com>
2026-01-27 20:48:21 +0600

    Remove temporary diagnostic scripts

--- check-mongo-store-v2.js
+++
@@ -1,6 +0,0 @@
-const MongoS = require('connect-mongo');
-console.log('MongoS.default:', MongoS.default);
-if (MongoS.default) console.log('MongoS.default.create:', typeof MongoS.default.create);
-
-console.log('MongoS.MongoStore:', MongoS.MongoStore);
-if (MongoS.MongoStore) console.log('MongoS.MongoStore.create:', typeof MongoS.MongoStore.create);
